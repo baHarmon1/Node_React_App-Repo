cb2b5e957bcce0e0da1a8efc2ffe3a77e8222e3a
Brycen Harmon <brycenharmon@gmail.com>
2022-03-11 18:44:42 -0600

    Created endpoint for csv file

--- server/index.js
+++ server/index.js
@@ -7,19 +7,26 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const results = [];
 
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send(results)
+})
+
 fs.createReadStream('emails3a89.CSV')
 .pipe(csv({}))
 .on('data', (data) => results.push(data))
 .on('end', () => {
+    
     console.log(results)
 })
 
-const app = express()
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json()) // convert to json format
 app.use('/', routesHandler)
 
 const PORT = 4000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
 })
